a84ccce646d89c79ee960313721d8f842a69f9d7
vyapakmishra1250 <vyapakdikmishra1250@gmail.com>
2021-04-17 19:48:46 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# VertoTweets
+Basic Twitter-like Social Network Using HTML, CSS, BOOTSTRAP, PHP, and AJAX
+
+
+@Mishraop
---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-push-3 footerBlock" style="padding:0px;">
+                <div class="container-fluid" id="about">
+                    <div class="row" style="background-image:url(Img/naturalblack.png)">
+                        <div class="col-md-4 col-xs-2">
+                            <i class="fa fa-twitter aboutIcon"></i>
+                        </div>
+                        <div class="col-md-8 col-xs-10" style="padding-left:30px;padding-bottom:0px;">
+                            <br>
+                            <p>Vertos Twitter Social platform</p>
+                            <p>Tell Vertos:</p>
+                            <p><a href="whatsapp://send?text=Guys... Check Out this new Website: Futatweets.com, My username is: <?php echo $_SESSION['username']?> !" data-action="share/whatsapp/share"><span class="fa fa-whatsapp"></span> Share via Whatsapp</a></p>
+                            
+                            <p> <a href="http://www.facebook.com/sharer.php?u=http://Futatweets.comxa.com" target="_blank"><span class="fa fa-facebook"></span> Share via Facebook</a></p>
+                            
+                            <p class="aboutInfo"><span class="glyphicon glyphicon-phone-alt"></span> <b>HelpDesk:</b> <a href="mailto:vyapakmishra25@gmail.com">admin@VertosTweets.com</a></p>
+
+                            <p>&copy; Vertos 2021</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</footer>
